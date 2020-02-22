0db81102911519f3e6e7bf6e82e5630832d82223
kipronoDenis <kipronokemei98@gmail.com>
2020-02-22 18:43:42 +0200

    Add react and home page design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,125 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+     <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+    <style type="text/css">
+      header {
+  position: relative;
+  background-color: black;
+  height: 75vh;
+  min-height: 25rem;
+  width: 100%;
+  overflow: hidden;
+}
+
+header video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: 0;
+  -ms-transform: translateX(-50%) translateY(-50%);
+  -moz-transform: translateX(-50%) translateY(-50%);
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+}
+
+header .container {
+  position: relative;
+  z-index: 2;
+}
+
+header .overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+  opacity: 0.5;
+  z-index: 1;
+}
+
+@media (pointer: coarse) and (hover: none) {
+  header {
+    background: url('https://source.unsplash.com/XT5OInaElMw/1600x900') black no-repeat center center scroll;
+  }
+  header video {
+    display: none;
+  }
+}
+
+.our-webcoderskull{
+
+  
+}
+.our-webcoderskull .cnt-block{ 
+   float:left; 
+   width:100%; 
+   background:#fff; 
+   padding:30px 20px; 
+   text-align:center; 
+   border:2px solid #d5d5d5;
+   margin: 0 0 28px;
+}
+.our-webcoderskull .cnt-block figure{
+   width:148px; 
+   height:148px; 
+   border-radius:100%; 
+   display:inline-block;
+   margin-bottom: 15px;
+}
+.our-webcoderskull .cnt-block img{ 
+   width:148px; 
+   height:148px; 
+   border-radius:100%; 
+}
+.our-webcoderskull .cnt-block h3{ 
+   color:#2a2a2a; 
+   font-size:20px; 
+   font-weight:500; 
+   padding:6px 0;
+   text-transform:uppercase;
+}
+.our-webcoderskull .cnt-block h3 a{
+  text-decoration:none;
+  color:#2a2a2a;
+}
+.our-webcoderskull .cnt-block h3 a:hover{
+  color:#337ab7;
+}
+.our-webcoderskull .cnt-block p{ 
+   color:#2a2a2a; 
+   font-size:13px; 
+   line-height:20px; 
+   font-weight:400;
+}
+.our-webcoderskull .cnt-block .follow-us{
+  margin:20px 0 0;
+}
+.our-webcoderskull .cnt-block .follow-us li{ 
+    display:inline-block; 
+  width:auto; 
+  margin:0 5px;
+}
+.our-webcoderskull .cnt-block .follow-us li .fa{ 
+   font-size:24px; 
+   color:#767676;
+}
+.our-webcoderskull .cnt-block .follow-us li .fa:hover{ 
+   color:#025a8e;
+}
+
+    </style>
 
 </head>
 <body>
     <div id="app px-4">
-      <nav class="navbar navbar-expand-lg shadow  text-white" style=" background: linear-gradient(to right, #2193b0, #6dd5ed);">
+      <nav class="navbar navbar-expand-lg shadow  text-white py-3" style=" background: linear-gradient(to right, #fc4a1a, #f7b733);">
         <a class="navbar-brand text-xl" href="/">Crime Reporting Sytem</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -37,6 +150,7 @@
           <!-- Right Side Of Navbar -->
           <ul class="navbar-nav ml-auto ">
               <!-- Authentication Links -->
+
               @guest
                   <li class="nav-item">
                       <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -81,5 +195,13 @@
             @yield('content')
         </main>
     </div>
+
+      <footer id="sticky-footer" class="py-4 bg-dark text-white-50"  style=" background: linear-gradient(to right, #fc4a1a, #f7b733);">
+        <div class="h-5"></div>
+    <div class="container text-center">
+      <small>Copyright &copy; Crime Reporting Sytem 2020</small>
+    </div>
+     <div class="h-5"></div>
+  </footer>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,241 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="h-screen w-full bg-white">
-<div class=" w-full  bg-gray-100 opacity-75 flex flex-column jusfify-center pt-10" >
+<header>
+  <div class="overlay"></div>
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="video/animation.mp4" type="video/mp4">
+  </video>
+  <div class="container h-100">
+    <div class="d-flex h-100 text-center align-items-center">
+      <div class="w-100 text-white">
+        <h1 class="display-3" style="color:#fc4a1a">Report Crime</h1>
+          
+        <p class="lead mb-0">Register to be able to use our platform</p>
+        <button class="btn btn-primary btn-lg shadow my-3" >Create an Account</button>
+      </div>
+    </div>
+  </div>
+</header>
 
-<div class="jumbotron w-3/4 mx-auto text-center">
-  <div class="h-10"></div>
-<h1 class="display-4 text-blue-900 mx-auto"> Do yo want a Report A Crime ?</h1>
-<p class="h4 text-primary mx-auto my-3">Lets the police know about you case now!!</p>
-<div class="flex w-full mt-5">
-<a  href="/report" class=" mx-2 w-1/2 btn btn-success py-3 shadow text-md opacity-100 ">Yes! I want</a>
-<a  href="/login" class=" mx-2 w-1/2 btn btn-primary py-3 shadow text-md opacity-100 ">No! I want to Login</a>
-</div>
-</div>
-</div>
-</div>
+<section class="mb-5 py-5">
+  <div class="container">
+
+    <div class="row bg-gray-200">
+
+      <div class="col-md-10 mx-auto">
+         <h1 class="display-4 text-center my-5" style="color:#fc4a1a">Wanted Criminals!!!</h1>
+          <p>The following people have been identified a the most wanted criminals and therefore the
+          National police service wishes to inform the public to submit their report here should they see any of the following. Rewards will be offered for any who find...</p>
+         <div class="h-10"></div>
+       <!-- Team -->
+<section id="team" class="pb-5 ">
+    <section class="our-webcoderskull padding-lg">
+  <div class="container">
+    <div class="row heading heading-icon">
+         
+    </div>
+    <ul class="row flex flex-wrap justify-around list-unstyled">
+      <li class="col-4 px-3 ">
+          <div class="cnt-block equal-hight border-red-500 border-1 rounded shadow " >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3 ><a href="http://www.webcoderskull.com/" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p>Armed Robber</p>
+           
+            <h3 ><a href="http://www.webcoderskull.com/" class="text-green-500 bold">Reward: Ksh. 1M</a></h3>
+            <div class='spacer h-10'></div>
+           <div class="mx-3 flex justify-between">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red">Report</a>
+           </div>
+          </div>
+      </li>
+      <li class="col-4 px-3">
+          <div class="cnt-block equal-hight border-red-500 " >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3 ><a href="http://www.webcoderskull.com/" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p>Armed Robber</p>
+           
+            <h3 ><a href="http://www.webcoderskull.com/" class="text-green-500 bold">Reward: Ksh. 1M</a></h3>
+            <div class='spacer h-10'></div>
+           <div class="mx-3 flex justify-between">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red">Report</a>
+           </div>
+          </div>
+      </li>
+
+      <li class="col-4 px-3">
+          <div class="cnt-block equal-hight border-red-500" >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3 ><a href="http://www.webcoderskull.com/" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p>Armed Robber</p>
+           
+            <h3 ><a href="http://www.webcoderskull.com/" class="text-green-500 bold">Reward: Ksh. 1M</a></h3>
+            <div class='spacer h-10'></div>
+           <div class="mx-3 flex justify-between">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red">Report</a>
+           </div>
+          </div>
+      </li>
+
+      <li class="col-4 px-3">
+          <div class="cnt-block equal-hight border-red-500" >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3 ><a href="http://www.webcoderskull.com/" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p>Armed Robber</p>
+           
+            <h3 ><a href="http://www.webcoderskull.com/" class="text-green-500 bold">Reward: Ksh. 1M</a></h3>
+            <div class='spacer h-10'></div>
+           <div class="mx-3 flex justify-between">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red">Report</a>
+           </div>
+          </div>
+      </li>   
+      <li class="col-4 px-3">
+          <div class="cnt-block equal-hight border-red-500" >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3 ><a href="http://www.webcoderskull.com/" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p>Armed Robber</p>
+           
+            <h3 ><a href="http://www.webcoderskull.com/" class="text-green-500 bold">Reward: Ksh. 1M</a></h3>
+            <div class='spacer h-10'></div>
+           <div class="mx-3 flex justify-between">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red">Report</a>
+           </div>
+          </div>
+      </li>
+
+      <li class="col-4 px-3">
+          <div class="cnt-block equal-hight border-red-500" >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3 ><a href="http://www.webcoderskull.com/" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p>Armed Robber</p>
+           
+            <h3 ><a href="http://www.webcoderskull.com/" class="text-green-500 bold">Reward: Ksh. 1M</a></h3>
+            <div class='spacer h-10'></div>
+           <div class="mx-3 flex justify-between">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red">Report</a>
+           </div>
+          </div>
+      </li>
+
+    </ul>
+  </div>
+</section>
+</section>
+      </div>
+    </div>
+
+        <div class="row mt-5">
+      <div class="col-md-8 mx-auto">
+      	 <h1 class="display-4" style="color:#fc4a1a">Missing Persons!!!</h1>
+        <p>The following persons went missing and are being sought for by their families and friends. The National Police service is therefore urging any one who has seen the person to report of their where abouts here.</p>
+        
+    
+      </div>
+    </div>
+        <div class="row mt-5">
+      <div class="col-md-9 mx-auto">
+       
+         <ul class="row flex flex-wrap justify-around list-unstyled">
+      <li class="col-4 px-3 ">
+          <div class="cnt-block equal-hight border-red-500 border-1 rounded shadow px-3" >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive card-img-top" alt="" ></figure>
+            <h3 class="my-2" >NAME: <a href="http://www.webcoderskull.com/" class="uppercase bolder my-2" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p class="my-2">Comes From:<strong>Meru County</strong></p>
+           
+            <h3 >Got Lost:<a href="http://www.webcoderskull.com/" class="text-green-500 bold">10 days Ago</a></h3>
+            <div class='spacer h-5'></div>
+           <div class="mx-3 flex justify-between my-2 mb-5">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red mb-2">Report</a>
+           </div>
+          </div>
+      </li>
+      <li class="col-4 px-3 ">
+          <div class="cnt-block equal-hight border-red-500 border-1 rounded shadow px-3" >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive card-img-top" alt="" ></figure>
+            <h3 class="my-2" >NAME: <a href="http://www.webcoderskull.com/" class="uppercase bolder my-2" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p class="my-2">Comes From:<strong>Meru County</strong></p>
+           
+            <h3 >Got Lost:<a href="http://www.webcoderskull.com/" class="text-green-500 bold">10 days Ago</a></h3>
+            <div class='spacer h-5'></div>
+           <div class="mx-3 flex justify-between my-2 mb-5">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red mb-2">Report</a>
+           </div>
+          </div>
+      </li>
+
+      <li class="col-4 px-3 ">
+          <div class="cnt-block equal-hight border-red-500 border-1 rounded shadow px-3" >
+            <figure><img src="http://www.webcoderskull.com/img/team3.png" class="img-responsive card-img-top" alt="" ></figure>
+            <h3 class="my-2" >NAME: <a href="http://www.webcoderskull.com/" class="uppercase bolder my-2" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p class="my-2">Comes From:<strong>Meru County</strong></p>
+           
+            <h3 >Got Lost:<a href="http://www.webcoderskull.com/" class="text-green-500 bold">10 days Ago</a></h3>
+            <div class='spacer h-5'></div>
+           <div class="mx-3 flex justify-between my-2 mb-5">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red mb-2">Report</a>
+           </div>
+          </div>
+      </li>
+
+      <li class="col-4 px-3 ">
+          <div class="cnt-block equal-hight border-red-500 border-1 rounded shadow px-3" >
+            <figure><img src="http://www.webcoderskull.com/img/team2.png" class="img-responsive card-img-top" alt="" ></figure>
+            <h3 class="my-2" >NAME: <a href="http://www.webcoderskull.com/" class="uppercase bolder my-2" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p class="my-2">Comes From:<strong>Meru County</strong></p>
+           
+            <h3 >Got Lost:<a href="http://www.webcoderskull.com/" class="text-green-500 bold">10 days Ago</a></h3>
+            <div class='spacer h-5'></div>
+           <div class="mx-3 flex justify-between my-2 mb-5">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red mb-2">Report</a>
+           </div>
+          </div>
+      </li>   
+      <li class="col-4 px-3 ">
+          <div class="cnt-block equal-hight border-red-500 border-1 rounded shadow px-3" >
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive card-img-top" alt="" ></figure>
+            <h3 class="my-2" >NAME: <a href="http://www.webcoderskull.com/" class="uppercase bolder my-2" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p class="my-2">Comes From:<strong>Meru County</strong></p>
+           
+            <h3 >Got Lost:<a href="http://www.webcoderskull.com/" class="text-green-500 bold">10 days Ago</a></h3>
+            <div class='spacer h-5'></div>
+           <div class="mx-3 flex justify-between my-2 mb-5">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red mb-2">Report</a>
+           </div>
+          </div>
+      </li>
+
+      <li class="col-4 px-3 ">
+          <div class="cnt-block equal-hight border-red-500 border-1 rounded shadow px-3" >
+            <figure><img src="http://www.webcoderskull.com/img/team1.png" class="img-responsive card-img-top" alt=""></figure>
+            <h3 class="my-2" >NAME: <a href="http://www.webcoderskull.com/" class="uppercase bolder my-2" style="color:#fc4a1a">Web coder skull</a></h3>
+            <p class="my-2">Comes From:<strong>Meru County</strong></p>
+           
+            <h3 >Got Lost:<a href="http://www.webcoderskull.com/" class="text-green-500 bold">10 days Ago</a></h3>
+            <div class='spacer h-5'></div>
+           <div class="mx-3 flex justify-between my-2 mb-5">
+             <a href="#" class="text-blue-500">Details</a>
+             <a href="#" class="btn btn-success border border-1 border-solig-red mb-2">Report</a>
+           </div>
+          </div>
+      </li>
+      
+    </ul>
+    
+      </div>
+    </div>
+
+  </div>
+</section>
 @endsection
